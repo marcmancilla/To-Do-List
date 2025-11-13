f59879391f9f2a52cc1ef1ae8b6985f007b09d20
marcm <mmancilla@elpuig.xeill.net>
2025-11-13 17:51:42 +0100

    Primeras funcionalidades dinamicas

--- Frontend/src/app/home/home.ts
+++ Frontend/src/app/home/home.ts
@@ -12,11 +12,58 @@ export class Home {
   tareas: any[] = [
     {
       titulo: "Mi primera tarea",
-      texto: "Texto de prueba primera tarea"
+      texto: "Texto de prueba primera tarea",
+      estado: "no-completada"
     },
     {
       titulo: "Segunda tarea",
-      texto: "Texto segunda tarea"
+      texto: "Texto segunda tarea",
+      estado: "no-completada"
+    },
+    {
+      titulo: "Tercera tarea",
+      texto: "Texto tercera tarea",
+      estado: "completada"
+    },
+    {
+      titulo: "Tercera tarea",
+      texto: "Texto tercera tarea",
+      estado: "no-completada"
+    },
+    {
+      titulo: "Tercera tarea",
+      texto: "Texto tercera tarea",
+      estado: "completada"
+    }
+    ,
+    {
+      titulo: "Tercera tarea",
+      texto: "Texto tercera tarea",
+      estado: "eliminada"
     }
   ];
+
+
+  filtro: string = "todas";
+
+  marcarCompletada(tarea: any) {
+    if (tarea.estado === "completada") {
+      tarea.estado = "no-completada";
+    } else if (tarea.estado === "no-completada") {
+      tarea.estado = "completada";
+    }
+  }
+
+  get tareasFiltradas() {
+    return this.tareas.filter(
+      t => this.filtro === 'todas' || t.estado === this.filtro
+    );
+  }
+
+
+  eliminarTarea(tarea: any) {
+    this.tareas = this.tareas.filter(t => t !== tarea);
+  }
+
 }
+
