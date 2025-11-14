cbaa41ca12e616dcc95daf514365e8049a03fa5e
marcm <mmancilla@elpuig.xeill.net>
2025-11-14 12:40:08 +0100

    Editar tarea

--- Frontend/src/app/home/home.ts
+++ Frontend/src/app/home/home.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   standalone: true,
   templateUrl: './home.html',
   styleUrl: './home.css',
@@ -13,33 +15,39 @@ export class Home {
     {
       titulo: "Mi primera tarea",
       texto: "Texto de prueba primera tarea",
-      estado: "no-completada"
+      estado: "no-completada",
+      editando: false
     },
     {
       titulo: "Segunda tarea",
       texto: "Texto segunda tarea",
-      estado: "no-completada"
+      estado: "no-completada",
+      editando: false
     },
     {
       titulo: "Tercera tarea",
       texto: "Texto tercera tarea",
-      estado: "completada"
+      estado: "completada",
+      editando: false
     },
     {
       titulo: "Tercera tarea",
       texto: "Texto tercera tarea",
-      estado: "no-completada"
+      estado: "no-completada",
+      editando: false
     },
     {
       titulo: "Tercera tarea",
       texto: "Texto tercera tarea",
-      estado: "completada"
+      estado: "completada",
+      editando: false
     }
     ,
     {
       titulo: "Tercera tarea",
       texto: "Texto tercera tarea",
-      estado: "eliminada"
+      estado: "eliminada",
+      editando: false
     }
   ];
 
@@ -63,6 +71,16 @@ export class Home {
 
   eliminarTarea(tarea: any) {
     this.tareas = this.tareas.filter(t => t !== tarea);
+  }
+
+
+  editarTarea(tarea: any) {
+    if (tarea.editando == false){
+      tarea.editando = true;
+    } else {
+      tarea.editando = false;
+    }
+    
   }
 
 }
